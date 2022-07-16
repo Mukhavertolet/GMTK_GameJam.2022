92e15c88ec9cfbc5fab4501171f36cc3625299ff
Mukhavertolet <Mukhavertolet@gmail.com>
2022-07-16 04:50:23 +0300

    Player movement limit

--- GMTK_GameJam.2022/GMTK_GameJam.2022/Assets/Scripts/PlayerMovement.cs
+++ GMTK_GameJam.2022/GMTK_GameJam.2022/Assets/Scripts/PlayerMovement.cs
@@ -18,19 +18,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if(Input.GetKeyDown(KeyCode.W) && transform.position.x != 0)
         {
             transform.Translate(-1, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) && transform.position.z != 0)
         {
             transform.Translate(0, 0, -1);
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S) && transform.position.x != 6)
         {
             transform.Translate(1, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D) && transform.position.z != 6)
         {
             transform.Translate(0, 0, 1);
         }
